4d801a7ad935e75961ae80f528a7a0cf929759d2
Joaquin Morcate <jmorcate@gmail.com>
2017-03-20 10:26:08 +0000

    Configured minitest reporters
    Configured minitest reports by adding the following lines to the test/test_helper.rb:
    	require 'minitest/reporters'
    	Minitest::Reporters.use!

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minintest::Reporters.use! [
+  Minitest::Reporters::ProgressReporter.new,
+  Minitest::Repprter::SpecReporter.new]
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
