f24521b3046c758172148db6b69866cc8ac52796
Joaquin Morcate <jmorcate@gmail.com>
2017-03-20 13:29:51 +0000

    Added static pages

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,11 @@
 module StaticPagesHelper
+    
+    def full_title(page_title="")
+        title_fixed_part ="The SSSB TDY Application"
+        if page_title.empty?
+            "#{title_fixed_part}"
+        else 
+            "#{page_title} | #{title_fixed_part}"
+        end
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @common_part_title = "The SSSB TDY Application"
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", full_title
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", full_title("Help")
   end
   
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", full_title("About")
   end
 end
