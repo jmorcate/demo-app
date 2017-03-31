65c7aa6cd1e646b82adc26fb62abcbfcf2c12c42
Joaquin Morcate <jmorcate@gmail.com>
2017-03-31 10:06:32 +0000

    Added automatic remembering users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,12 +2,31 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remeber_token)
+    end
     def log_out 
+        forget(@current_user)
         session.delete(:user_id)
         @current_user = nil
     end
     def current_user 
-       @current_user ||=  User.find_by(id: session[:user_id]) 
+        if (session[:user_id])
+            @current_user ||=  User.find_by(id: session[:user_id]) 
+        elsif cookies.signed[:user_id] 
+            user = find_by(id: cookies.signed[:user_id])
+            if (user && user.authenticathed?(cookies[:remember_token]))
+                log_in user
+                @current_user = user
+            end
+        end
     end
     
     def logged_in? 
